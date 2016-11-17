a9201d37bf81b6d8a156d6df96ddf720fac0ff5e
Christian <cguerrero@hmc.edu>
2016-11-16 20:36:08 -0600

    add js files for the youtubeClipCreator and js playground

---
+++ js/myscripts.js
@@ -0,0 +1,18 @@
+function whoIsIt(){
+	var person = prompt("Please enter your name here");
+	if (person == "Christian"){
+		alert("Hello Christian!");
+	}
+	else if (person == "Ashley" || "ashely"){
+		alert("You're a loser!")
+	}
+	else if (person == "Amy" || "amy"){
+	alert("You're a loser!")
+	}
+	else if (person == "Lindha" || "lindha" || "lindha"){
+	alert("You're a loser!")
+	}
+	else {
+		alert("You are not Christian! You must not be cool." );
+	}
+}
