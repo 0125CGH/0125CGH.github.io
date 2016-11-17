d1637b896e88c7cc1cd11b65576efac4fb450df0
Christian <cguerrero@hmc.edu>
2016-11-16 21:06:06 -0600

    update myscripts.js function

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,18 +1,18 @@
 function whoIsIt(){
 	var person = prompt("Please enter your name here");
-	if (person == "Christian"){
+	if (person == "Christian" || "christian"){
 		alert("Hello Christian!");
 	}
 	else if (person == "Ashley" || "ashely"){
-		alert("You are not Christian! You must not be cool.")
+		alert("You are not Christian!")
 	}
 	else if (person == "Amy" || "amy"){
-		alert("You are not Christian! You must not be cool.")
+		alert("You are not Christian!")
 	}
 	else if (person == "Lindha" || "lindha" || "lindha"){
-		alert("You are not Christian! You must not be cool.")
+		alert("You are not Christian!")
 	}
 	else {
-		alert("You are not Christian! You must not be cool." );
+		alert("You are not Christian!" );
 	}
 }
